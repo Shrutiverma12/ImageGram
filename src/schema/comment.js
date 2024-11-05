bc249c805561a45b49e0bba90fc5200b64a300fe
Shruti Verma <shrutive1009@gmail.com>
2024-11-05 18:47:15 +0530

    Added comment and like schema

---
+++ src/schema/comment.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    required: true,
+    minLength: 1,
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+  onModel: {
+    type: String,
+    required: true,
+    enum: ["Post", "Comment"],
+  },
+  commentableId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    refPath: "onModel",
+  },
+  replies: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment",
+    },
+  ],
+});
+
+const comment = mongoose.model("Comment", commentSchema);
+export default comment;
